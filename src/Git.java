a8e1c83c7fc3a1cd9662a0b121af147d5a576680
Manuel <molada11@gmail.com>
2022-05-25 11:32:10 +0200

    Hago commit del proyecyo

---
+++ src/Git.java
@@ -0,0 +1,27 @@
+
+public class Git {
+	
+	public static void main (String[]args) { //*Metodo Principal*//
+		String [] personas = {"Ana","Luis","Antonio","Manuel"};
+		
+	}
+
+	public static void ContarVocales(String [] personas) { //* Metodo que usamos para contar vocales *//
+		int a=4;
+		int e=1;
+		int ii=2;
+		int o=2;
+		int u=2;
+		
+		for (int i=0; i<personas.length;i++) {
+			String palabra;
+			palabra = personas[i];
+		}
+			for (int i=0; i<personas.length;i++) {
+				
+			}
+		
+		
+		
+	}
+}
